628d6d136c94d5424b74a8e5b33f4d8aaefe3c3e
Kacper Troscianka <40548840+kacpertroscianka@users.noreply.github.com>
2026-07-05 00:34:53 +0200

    Update luafunctions_client.cpp

--- src/client/luafunctions_client.cpp
+++ src/client/luafunctions_client.cpp
@@ -499,6 +499,10 @@ void Client::registerLuaFunctions()
     g_lua.registerClass<Creature, Thing>();
     g_lua.bindClassStaticFunction<Creature>("create", []{ return CreaturePtr(new Creature); });
     g_lua.bindClassMemberFunction<Creature>("getId", &Creature::getId);
+    g_lua.bindClassMemberFunction<Creature>("setDrawOffset", &Creature::setDrawOffset);
+    g_lua.bindClassMemberFunction<Creature>("getDrawOffset", &Creature::getDrawOffset);
+    g_lua.bindClassMemberFunction<Creature>("setOutfitShader", &Creature::setOutfitShader);
+    g_lua.bindClassMemberFunction<Creature>("getOutfitShader", &Creature::getOutfitShader);
     g_lua.bindClassMemberFunction<Creature>("getName", &Creature::getName);
     g_lua.bindClassMemberFunction<Creature>("setName", &Creature::setName);
     g_lua.bindClassMemberFunction<Creature>("setManaPercent", &LocalPlayer::setManaPercent);
